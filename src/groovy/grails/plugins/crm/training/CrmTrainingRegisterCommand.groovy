b8e96541c4ffe46f9f1adf4b50e59cacd6c49203
Goran Ehrsson <goran@technipelago.se>
2015-03-22 08:30:19 +0100

    Added properties (country, ice1-3, tags)

--- src/groovy/grails/plugins/crm/training/CrmTrainingRegisterCommand.groovy
+++ src/groovy/grails/plugins/crm/training/CrmTrainingRegisterCommand.groovy
@@ -16,9 +16,14 @@ class CrmTrainingRegisterCommand implements Serializable {
     String address1
     String postalCode
     String city
+    String country
     String email
     String telephone
+    String ice1
+    String ice2
+    String ice3
     String msg
+    String[] tags
 
     static constraints = {
         number(maxSize: 40, blank: false)
@@ -29,9 +34,14 @@ class CrmTrainingRegisterCommand implements Serializable {
         address1(maxSize: 255, nullable: true)
         postalCode(maxSize: 10, nullable: true)
         city(maxSize: 40, nullable: true)
+        country(maxSize: 40, nullable: true)
         email(maxSize: 80, blank: false)
         telephone(maxSize: 40, nullable: true)
+        ice1(maxSize: 80, nullable: true)
+        ice2(maxSize: 80, nullable: true)
+        ice3(maxSize: 80, nullable: true)
         msg(maxSize: 2000, nullable: true, widget: 'textarea')
+        tags(nullable: true)
     }
 
     static transients = ['address']
@@ -53,16 +63,27 @@ class CrmTrainingRegisterCommand implements Serializable {
             }
             s << city
         }
+        if (country) {
+            if (s.length()) {
+                s << ' '
+            }
+            s << country
+        }
         s.toString()
     }
 
     Map<String, String> asMap() {
-        ['number', 'firstName', 'lastName', 'title', 'company', 'address', 'address1', 'postalCode', 'city', 'email', 'telephone', 'msg'].inject([:]) { map, prop ->
+        def result = ['number', 'firstName', 'lastName', 'title', 'company', 'address', 'address1', 'postalCode', 'city', 'country',
+                      'email', 'telephone', 'msg', 'ice1', 'ice2', 'ice3'].inject([:]) { map, prop ->
             def value = this[prop]
             if (value != null) {
                 map[prop] = value
             }
             return map
         }
+        if (tags) {
+            result.tags = tags.toList()
+        }
+        return result
     }
 }
