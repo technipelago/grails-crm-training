f964d513a64be394cc307d4f9b9e99b28042e044
Goran Ehrsson <goran@technipelago.se>
2016-08-14 23:06:32 +0200

    Version 2.4.1

--- CrmTrainingGrailsPlugin.groovy
+++ CrmTrainingGrailsPlugin.groovy
@@ -18,7 +18,7 @@ import grails.plugins.crm.training.CrmTraining
 
 class CrmTrainingGrailsPlugin {
     def groupId = ""
-    def version = "2.4.1-SNAPSHOT"
+    def version = "2.4.1"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def loadAfter = ['crmTags']
