4f975fdcb170f1e0e4dff1e33877e07e4c53fe91
Goran Ehrsson <goran@technipelago.se>
2025-05-06 21:00:35 +0200

    feat: Update plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -41,13 +41,13 @@ grails.project.dependency.resolution = {
         test(":code-coverage:2.0.3-3") { export = false }
 
         compile ":crm-core:2.4.3"
-        compile ":crm-security:2.4.4"
-        compile ":crm-tags:2.4.2"
-        compile ":crm-contact:2.4.2"
-        compile ":crm-task:2.4.5"
+        compile ":crm-security:2.4.5"
+        compile ":crm-tags:2.4.3"
+        compile ":crm-contact:2.4.5"
+        compile ":crm-task:2.5.0"
 
-        compile ":sequence-generator:1.2"
-        compile ":selection:0.9.8"
+        compile ":sequence-generator:1.2.1"
+        compile ":selection:0.9.9"
     }
 }
 
